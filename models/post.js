64343c09cfcac14a90995ea4d78ac061d36bc3aa
Naresh Sharma <nareshsharma607@gmail.com>
2021-06-23 08:28:52 +0530

    blog

---
+++ models/post.js
@@ -0,0 +1,18 @@
+const mongoose=require('mongoose');
+
+const postSchema=new mongoose.Schema({
+    content:{
+        type:String,
+    required:true
+},
+title:{
+    type:String,
+    required:true
+
+}},
+
+{timestamps:true
+})
+
+const Post=mongoose.model('Post',postSchema)
+module.exports=Post
